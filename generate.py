72434c7ff4124cfd7a01dcb9b1b0fa9cb1cd13ca
Ali Astin Azarafza <astinazarafza@gmail.com>
2023-12-27 12:50:05 -0500

    An adjustment to the 'img =' cv2 call
    An adjustment to the 'img =' cv2 call to still execute in a different newer python environment, instead of failing

--- generate.py
+++ generate.py
@@ -92,7 +92,10 @@ for idx, path in enumerate(images, 1):
     os.makedirs(output_dir, exist_ok=True)
     print(idx, base)
     # read image
-    img = cv2.imread(path, cv2.cv2.IMREAD_COLOR)
+    try: 
+        img = cv2.imread(path, cv2.cv2.IMREAD_COLOR)
+    except:
+        img = cv2.imread(path, cv2.IMREAD_COLOR)
 
     # Seamless modes
     if args.seamless:
